43683444ca4c440aedcc7c0ded3b4bb70083e5dd
DataNick <DataNick@users.noreply.github.com>
2015-07-01 16:39:46 -0400

    Changes to production and development environments to set up action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,15 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+
+  config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'yourdomain.com'
+  }
+  
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
