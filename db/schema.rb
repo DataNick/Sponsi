20705ffcd4921c52716aece73d26ac3ea45da353
DataNick <DataNick@users.noreply.github.com>
2015-07-01 17:15:33 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701201829) do
+ActiveRecord::Schema.define(version: 20150701205712) do
 
   create_table "applications", force: true do |t|
     t.string   "name"
@@ -60,8 +60,13 @@ ActiveRecord::Schema.define(version: 20150701201829) do
     t.boolean  "admin",                  default: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
