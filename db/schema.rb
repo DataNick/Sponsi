43f7ab73ffdc61a83bce5accd6af92f6939843e1
DataNick <DataNick@users.noreply.github.com>
2015-07-01 16:34:58 -0400

    Update schema by running migration on AddConfirmableToDevise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630181746) do
+ActiveRecord::Schema.define(version: 20150701201829) do
 
   create_table "applications", force: true do |t|
     t.string   "name"
