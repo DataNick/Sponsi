d99da6ad61e544545817023ba11ed81800e3a27c
Chris-Bishop <christopher.allan.bishop@gmail.com>
2015-07-03 13:26:31 -0400

    Fixing Migrations with Application

--- db/migrate/20150703142917_remove_sponsorship_id_from_applications.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSponsorshipIdFromApplications < ActiveRecord::Migration
-  def change
-    remove_column :applications, :sponsorship_id, :integer
-  end
-end
--- db/migrate/20150703143127_add_reward_id_to_applications.rb
+++
@@ -1,5 +0,0 @@
-class AddRewardIdToApplications < ActiveRecord::Migration
-  def change
-    add_column :applications, :reward_id, :integer
-  end
-end
