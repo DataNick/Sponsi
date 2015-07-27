bc5b4761a1c9a8f100be947f05ec469eb6d07143
DataNick <DataNick@users.noreply.github.com>
2015-07-27 14:49:01 -0400

    Set up sign_up/login as root route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,15 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'sponsorships#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 
   resources :users, only: [:edit, :update, :index, :show]
 
