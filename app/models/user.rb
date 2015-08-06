be9dea2c04d0b54e1e0ee6e44e3466fd391c9aa0
DataNick <DataNick@users.noreply.github.com>
2015-08-06 10:13:08 -0400

    setting up confirmation false and production site

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :large => "800x500", :medium => "300x300>", :thumb => "150x150#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+  protected
+  def confirmation_required?
+    false
+  end
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
 
-  config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
+  config.action_mailer.default_url_options = {:host => 'sponsi.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
